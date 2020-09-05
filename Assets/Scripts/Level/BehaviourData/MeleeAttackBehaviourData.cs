ade740d156755b84616c2ad1f47a64ccaffbf64e
Alligatooo <rittner.benni@t-online.de>
2020-09-06 01:47:53 +0200

    Auto stash before merge of "main" and "origin/main"

--- Assets/Scripts/Level/BehaviourData/MeleeAttackBehaviourData.cs
+++ Assets/Scripts/Level/BehaviourData/MeleeAttackBehaviourData.cs
@@ -3,6 +3,7 @@
 [CreateAssetMenu(fileName = "New Melee Attack Behaviour Data", menuName = "NeonDefence/MeleeAttackBehaviourData")]
 public class MeleeAttackBehaviourData : BehaviourData
 {
+    public MeeleBullet bulletPrefab;
     public int range;
     public float attackThreshold;
 }
