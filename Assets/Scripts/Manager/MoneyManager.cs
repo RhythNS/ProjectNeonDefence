6fdba8bf3ded05b6a6d19d54406a393d4840dcb2
Rhyth <rhythns@gmail.com>
2020-09-05 17:03:52 +0200

    .

---
+++ Assets/Scripts/Manager/MoneyManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class MoneyManager : MonoBehaviour
+{
+    public static MoneyManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public int CurrentMoney { get; private set; }
+
+    public void EnemyKilled(Enemy enemy)
+    {
+        //TODO: the thing
+    }
+
+    public bool CanPlaceTower(Tower tower)
+    {
+        // TODO: the thing
+        return true;
+    }
+}
