4944cf414b930a3a53d2570b2529a6368e508a34
Alligatooo <rittner.benni@t-online.de>
2020-09-05 05:07:00 +0200

    Basic enemy v7
    - Added RangedAttackBehaviour with LaserBullet

---
+++ Assets/Scripts/Enemy/RangedAttackTankBehaviour.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangedAttackBehaviour : MonoBehaviour, Behaviour
+{
+
+    [SerializeField] private int range;
+
+    [SerializeField] private LaserBullet laserPrefab;
+
+    [SerializeField] private float attackThreshold;
+    public Tower AttackingTower { get; private set; }
+
+
+    public void Set()
+    {
+        //TODO @Noah/RhythNS
+        //attackThreshold = data.attackThreshold;
+        //range = data.range;
+    }
+
+    public void OnNewTileEntered(Tile tile)
+    {
+
+    }
+
+
+    private IEnumerator UpdateAttack()
+    {
+        while (true)
+        {
+            if (AttackingTower == null || !AttackingTower)
+                yield return null;
+            ShootMissileAt(AttackingTower);
+            yield return new WaitForSeconds(attackThreshold);
+        }
+    }
+
+    private void ShootMissileAt(Tower attackingTower)
+    {
+        //TODO mit Noah besprechen, wie Laserbeam dargestellt werden soll? 
+
+
+        LaserBullet laserBullet = Instantiate<LaserBullet>(laserPrefab);
+        laserBullet.Target = attackingTower;
+    }
+}
