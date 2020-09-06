997cadf03cc467f03e081f1bf8f6552e5ace29c7
Wesson <blackhole@live.de>
2020-09-06 13:58:57 +0200

    Game over scene

--- Assets/Scripts/UI/GameOverManager.cs
+++ Assets/Scripts/UI/GameOverManager.cs
@@ -1,18 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameOverManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void RestartGame()
     {
-        
+        SceneManager.LoadScene("SampleScene");
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public void QuitGame()
     {
-        
+        Application.Quit();  
     }
 }
