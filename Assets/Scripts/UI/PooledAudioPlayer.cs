f60ef53864c9818be3f241defff406bcc3be96c4
Wesson <blackhole@live.de>
2020-09-06 16:05:09 +0200

    Music

---
+++ Assets/Scripts/UI/PooledAudioPlayer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PooledAudioPlayer : MonoBehaviour
+{
+    public AudioClip[] clips;
+
+    public AudioSource source;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayRandomFromPool();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void PlayRandomFromPool()
+    {
+        int rand = Random.Range(0, clips.Length);
+        source.clip = clips[rand];
+        source.Play();
+    }
+}
